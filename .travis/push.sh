b02283773a1740b1f06d18f7b3be20cdb3776a04
Daniel Gibbs <me@danielgibbs.co.uk>
2018-09-07 19:07:49 +0100

    added xml

--- .travis/push.sh
+++ .travis/push.sh
@@ -7,6 +7,7 @@ git remote set-url origin https://dgibbs64:${GH_TOKEN}@github.com/dgibbs64/Steam
 
 git checkout ${TRAVIS_BRANCH}
 git add . steamcmd_appid.json
+git add . steamcmd_appid.xml
 git add . steamcmd_appid.csv
 git add . steamcmd_appid.md
 git commit --message "Travis build: $(date +%Y-%m-%d)"
