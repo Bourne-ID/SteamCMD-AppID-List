83f43736a9621c9b0b622651c85c1382fb337435
Daniel Gibbs <me@danielgibbs.co.uk>
2018-10-13 19:13:53 +0100

    test

--- steamcmd_appid.sh
+++ steamcmd_appid.sh
@@ -35,8 +35,10 @@ fi
 for row in $( cat "${rootdir}/steamcmd_appid.json" | jq '.applist.apps[]' | jq -r '.appid'); do
     subscription=$("${rootdir}/steamcmd"/steamcmd.sh +login anonymous +app_status ${row} +exit | grep Subscribed | wc -l);
     if [ "${subscription}" == "1" ]; then
-        echo "anon sub available: ${row}";
-    fi;
+        echo "anonymous sub available: YES: ${row}";
+    else
+    	echo "anonymous sub available: NO: ${row}";
+    fi
 done
 
 echo "exit"
