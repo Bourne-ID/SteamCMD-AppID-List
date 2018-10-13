6a19c8f0eb4411014bd0320a06ade655d96c6329
Daniel Gibbs <me@danielgibbs.co.uk>
2018-10-13 01:36:07 +0100

    test

--- steamcmd_appid.sh
+++ steamcmd_appid.sh
@@ -14,5 +14,6 @@ curl https://api.steampowered.com/ISteamApps/GetAppList/v2/?format=xml > steamcm
 echo "Creating steamcmd_appid.csv"
 cat steamcmd_appid.json | jq '.applist.apps[]' | jq -r '[.appid, .name] | @csv' > steamcmd_appid.csv
 cat steamcmd_appid.json | jq '.applist[]' | md-table > steamcmd_appid.md
+steamcmd.sh +login anonymous +app_status 215360 +exit|grep Subscribed
 echo "exit"
 exit
