f95267665ce4b9b517e87dd61b781ccc9c4055cc
BourneID <cam40902@gmail.com>
2018-11-22 12:29:57 -0600

    Fix git add

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,11 +6,11 @@ git config --global user.name "Bourne-ID"
 git remote set-url origin https://Bourne-ID:${GH_TOKEN}@github.com/Bourne-ID/SteamCMD-AppID-List.git
 
 git checkout ${TRAVIS_BRANCH}
-git add . steamcmd_appid.json
-git add . steamcmd_appid.xml
-git add . steamcmd_appid.csv
-git add . steamcmd_appid.md
-git add . steamcmd_appid_anon.json
+git add steamcmd_appid.json
+git add steamcmd_appid.xml
+git add steamcmd_appid.csv
+git add steamcmd_appid.md
+git add steamcmd_appid_anon.json
 git commit --message "Travis build: $(date +%Y-%m-%d)"
 
 
