0ef43bd4ace72ee75092795481defa63910ea41d
Robin Bourne <cam40902@gmail.com>
2018-11-30 17:10:39 +0000

    fix md generation and stream clashes

--- steamcmd_appid.sh
+++ steamcmd_appid.sh
@@ -6,10 +6,12 @@
 # Description: Saves the complete list of all the appid their names in json and csv and produces a anonymous server list
 # env var TMUX_SESSIONS should be set.
 
+TMUX_SESSIONS=4
+
 rootdir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 echo "Creating steamcmd_appid.json"
-curl https://api.steampowered.com/ISteamApps/GetAppList/v2/ | jq -r '.' > steamcmd_appid.json
+curl https://api.steampowered.com/ISteamApps/GetAppList/v2/ | jq -r '.' > steamcmd_getapplist.json
 echo "Creating steamcmd_appid.xml"
 curl https://api.steampowered.com/ISteamApps/GetAppList/v2/?format=xml > steamcmd_appid.xml
 # echo "Creating steamcmd_appid.csv"
@@ -17,7 +19,7 @@ curl https://api.steampowered.com/ISteamApps/GetAppList/v2/?format=xml > steamcm
 # cat steamcmd_appid.json | jq '.applist[]' | md-table > steamcmd_appid.md
 
 # prep the tmux command file for steamcmd
-cat steamcmd_appid.json | jq '.applist.apps[]' | jq -r '[.appid] | @csv' | sed 's/^/tmux send-keys "app_status /' | sed 's/$/" ENTER/' > tmux_commands.sh
+cat steamcmd_getapplist.json | jq '.applist.apps[]' | jq -r '[.appid] | @csv' | sed 's/^/tmux send-keys "app_status /' | sed 's/$/" ENTER/' > tmux_commands.sh
 # Split the commands into the ENV for the number of sessions - names of the files will be tmuxXX.sh
 split --numeric-suffixes=1 -n l/${TMUX_SESSIONS} --additional-suffix=.sh tmux_commands.sh tmux
 # Alter each split file to name the relevant session which will be created later
@@ -111,16 +113,18 @@ jq -Rsn '
 
 # Merge the tmux files and generate CSV and MD files
 
-jq -s '[ .[0].applist.apps + .[1].applist.apps | group_by(.appid)[] | add]' steamcmd_appid.json tmuxallout.json > steamcmd_appid.json
+jq -s '[ .[0].applist.apps + .[1].applist.apps | group_by(.appid)[] | add]' steamcmd_getapplist.json tmuxallout.json > steamcmd_appid.json
 cat steamcmd_appid.json | jq '.[] |  select(.subscription == "released (Subscribed,Permanent,)") ' > steamcmd_appid_anon_servers.json
 cat steamcmd_appid_anon_servers.json | jq -r '[.appid, .name, .subscription] | @csv' > steamcmd_appid_anon_servers.csv
-cat steamcmd_appid_anon_servers.json | jq -r '.' | md-table > steamcmd_appid_anon_servers.md
+cat steamcmd_appid_anon_servers.json | jq -s '.' | md-table > steamcmd_appid_anon_servers.md
 
 echo "Creating steamcmd_appid.csv"
 cat steamcmd_appid.json | jq '.[]' | jq -r '[.appid, .name, .subscription] | @csv' > steamcmd_appid.csv
-cat steamcmd_appid.json | jq '.[]' | md-table > steamcmd_appid.md
+cat steamcmd_appid.json | jq -s '.' | md-table > steamcmd_appid.md
+
 # Clean up
 rm tmux*
+rm steamcmd_getapplist.json
 
 echo "exit"
 exit
